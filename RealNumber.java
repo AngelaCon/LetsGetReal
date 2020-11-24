f814beb22a984faa625b3cf17bbd0e086166e2d9
Angela Cai <angelacai@Acais-Mac.T-mobile.com>
2020-11-23 20:31:12 -0500

    file set

--- RealNumber.java
+++ RealNumber.java
@@ -13,6 +13,34 @@ public class RealNumber {
     return ""+getValue();
   }
 
+  public boolean equals(RealNumber other){
+      boolean result = false;
+      double a = this.getValue();
+      double b = other.getValue();
+      double valueA = (Math.abs(a-b)/a)*100;
+      double valueB = (Math.abs(b-a)/b)*100;
+      if (a == 0 && b == 0) return result =true;
+      if (valueA< 0.00001 && valueA < 0.00001) return result=true;
+      return result;
+  }
+
+  public RealNumber add(RealNumber other){
+     return new RealNumber(this.getValue() + other.getValue());
+  }
+
+  public RealNumber multiply(RealNumber other){
+    return new RealNumber(this.getValue() * other.getValue());
+  }
+
+  public RealNumber divide(RealNumber other){
+       return new RealNumber(this.getValue()/ other.getValue());
+ }
+
+
+  public RealNumber subtract(RealNumber other){
+    return new RealNumber(this.getValue() - other.getValue());
+  }
+
 
 
 }
